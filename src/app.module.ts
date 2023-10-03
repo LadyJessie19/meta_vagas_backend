583c6b0ec264e72b167de5d32709f82ff25e6ebc
Jessie <jessie.moura19@gmail.com>
2023-10-03 16:26:36 -0300

    feat: database - database initial configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.config.ts
@@ -0,0 +1,26 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+export default <TypeOrmModuleAsyncOptions>{
+  imports: [ConfigModule],
+  inject: [ConfigService],
+
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => {
+    return <PostgresConnectionOptions>{
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: +configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_DATABASE'),
+      entities: [],
+      synchronize: true,
+    };
+  },
+};
