7ca0115379f82cf7cac0a014f8afffa468d61256
Jessie <jessie.moura19@gmail.com>
2023-10-05 01:25:04 -0300

    feat: roles - added the roles files

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { ROLES_KEY } from '../../decorators/roles.decorator';
+import { RoleEnum } from '../../enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<RoleEnum[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    return requiredRoles.some((role) => user.role?.includes(role));
+  }
+}
---
+++ src/enums/role.enum.ts
@@ -0,0 +1,5 @@
+export enum RoleEnum {
+    student = 'student',
+    instructor = 'instructor',
+    admin = 'admin',
+}
