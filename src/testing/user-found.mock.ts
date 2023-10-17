75e7d00e872522bbac465cd5f3c650718c8a5f78
Calebe <calebe.oliveir@gmail.com>
2023-10-16 23:46:47 -0300

    feat/COFerr-tasks

---
+++ src/testing/user-found.mock.ts
@@ -0,0 +1,7 @@
+import { CreateUserDto } from "src/user/dto/create.user.dto";
+
+export const UserFoundMock : CreateUserDto = {
+    name : "teste",
+    email : "jtest@arnia.com",
+    password : "123456",
+}
